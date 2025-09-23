67d7f63918b88a943e5f6c4e7a473a0c9bca2d5e
Rahul Kurmalkar <therahulkurmalkar@gmail.com>
2025-09-23 16:50:05 +0530

    downgread express version

--- backend/server.js
+++ backend/server.js
@@ -35,13 +35,13 @@ if (process.env.NODE_ENV === "production") {
     app.use(express.static(path.join(__dirname, "/frontend/dist")));
 
     // other than the above routes.
-    // app.use("*", (req, res) => {
-    //     res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
-    // })
-    app.get('*', (req, res, next) => {
-        if (req.path.startsWith('/api')) return next();
-        res.sendFile(path.resolve(__dirname, 'frontend', 'dist', 'index.html'));
-    });
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+    })
+    // app.get('*', (req, res, next) => {
+    //     if (req.path.startsWith('/api')) return next();
+    //     res.sendFile(path.resolve(__dirname, 'frontend', 'dist', 'index.html'));
+    // });
 
 }
 
