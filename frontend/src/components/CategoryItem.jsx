85a39c09dd70586302defd23102e38ca45a77474
Rahul Kurmalkar <therahulkurmalkar@gmail.com>
2025-09-24 10:15:06 +0530

    category homepage image removed lazy loading

--- frontend/src/components/CategoryItem.jsx
+++ frontend/src/components/CategoryItem.jsx
@@ -10,7 +10,6 @@ const CategoryItem = ({category}) => {
 						src={category.imageUrl}
 						alt={category.name}
 						className='w-full h-full object-cover transition-transform duration-500 ease-out group-hover:scale-110'
-						loading='lazy'
 					/>
 					<div className='absolute bottom-0 left-0 right-0 p-4 z-20'>
 						<h3 className='text-white text-2xl font-bold mb-2'>{category.name}</h3>
